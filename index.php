f219f1f0711bb844a1ba895b9b177d300a1ec604
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-17 12:59:47 +0800

    add: arrays

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+ //   define('NAME', 'Mark')
+
+ // multi-dimensionnal arrays more like an objects
+
+ $blogs = [
+    ['mario party', 'mario', 'lorem', 30],
+    ['mario kart chets', 'toad', 'lorem', 25],
+    ['zelda hidden chest', 'link', 'lorem', 50]
+ ];
+
+ var_dump($blogs);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1</h1>
+    </body>
+</html>
