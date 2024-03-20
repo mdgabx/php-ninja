5a5669d3be29b91167e5c5d92325f89761f1dbcf
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-20 22:50:05 +0800

    add: header and footer

--- index.php
+++ index.php
@@ -1,49 +1,11 @@
-<?php 
- //   define('NAME', 'Mark')
-
- // multi-dimensionnal arrays more like an objects
-
-//  $blogs = [
-//     ['mario party', 'mario', 'lorem', 30],
-//     ['mario kart chets', 'toad', 'lorem', 25],
-//     ['zelda hidden chest', 'link', 'lorem', 50]
-//  ];
-
-//  $names = ['Art', 'Hello', 'World'];
-
-//  foreach($names as $n) {
-//     echo "test" . $n;
-//  }
-
-//  $products = ["item" => "shoes", "price" => 300];
-
-//  function state($product) {
-//     return "{$product['item']} and {$product['price']}";
-//  }
-
-// $result = state($products);
-// ;
-// echo $result
-
-$name = 'mark';
-
-function sayBye(&$name) {
-   //  global $name access the global variable outside the scope
-
-    $name = 'hello';
-    echo "Bye $name";
-}
-
-sayBye($name);
-//echo $name;
-
-//  var_dump($blogs);
+<?php
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <body>
-        <h1</h1>
-    </body>
+
+    <?php include('./templates/header.php'); ?>
+
+    <?php include('./templates/footer.php'); ?>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<head>
+    <title>Pizza Project</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <style type="text/css">
+        .brand {
+            background: brown;
+        }
+
+        .brand-text {
+            color: brown !important;
+        }
+    </style>
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Pizza House</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
+        </div>
+    </nav>
