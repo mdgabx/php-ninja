e3b52ca8571e6e60b5268afaac9a56fe616ca7a8
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-22 22:37:22 +0800

    add: test

--- add.php
+++ add.php
@@ -1,33 +1,55 @@
 <?php
 
-    if(isset($_POST['submit'])) {
-        var_dump($_POST);
+if (isset($_POST['submit'])) {
+    // var_dump($_POST);
+
+    // check email
+    if (empty($_POST['email'])) {
+        echo 'An email is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['email']);
     }
+
+    // check title
+    if (empty($_POST['title'])) {
+        echo 'An title is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['title']);
+    }
+
+    // check ingredients
+    if (empty($_POST['ingredients'])) {
+        echo 'An ingredients is required <br />';
+    } else {
+        echo htmlspecialchars($_POST['ingredients']);
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 
-    <?php include('./templates/header.php'); ?>
+<?php include('./templates/header.php'); ?>
 
-    <section class="container grey-text">
-        <h4 class="center">
-            Add a Pizza
-        </h4>
-        <form class="white" action="add.php" method="POST">
-            <label>Your Email:</label>
-            <input type="text" name="email" />
-            <label>Pizza Title:</label>
-            <input type="text" name="title" />
-            <label>Ingredients (comma separated):</label>
-            <input type="text" name="ingredients" />
+<section class="container grey-text">
+    <h4 class="center">
+        Add a Pizza
+    </h4>
+    <form class="white" action="add.php" method="POST">
+        <label>Your Email:</label>
+        <input type="text" name="email" />
+        <label>Pizza Title:</label>
+        <input type="text" name="title" />
+        <label>Ingredients (comma separated):</label>
+        <input type="text" name="ingredients" />
 
-            <div class="center">
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0" />
-            </div>
-        </form>
-    </section>
+        <div class="center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0" />
+        </div>
+    </form>
+</section>
 
 
-    <?php include('./templates/footer.php'); ?>
+<?php include('./templates/footer.php'); ?>
+
 </html>
