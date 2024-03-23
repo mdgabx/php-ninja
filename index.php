bab75f72bbf9f9400d18d44dfb819fd6bb87911f
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-23 22:00:51 +0800

    add: fetch data in php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     // query to fetch all pizzas
 
-    $sql = "SELECT title, ingredients, id FROM pizza_house.pizzas";
+    $sql = "SELECT title, ingredients, id FROM pizza_house.pizzas ORDER BY created_at DESC";
 
     // get the result
 
@@ -24,15 +24,33 @@
     //close connection
     mysqli_close($conn);
 
-    print_r($pizzas);
-
-
 ?>
 
 <!DOCTYPE html>
 <html>
 
     <?php include('./templates/header.php'); ?>
+
+    <h4 class="center grey-text">Pizzas!</h4>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza){ ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?= htmlspecialchars($pizza['title']) ; ?></h6>
+                            <div><?= htmlspecialchars($pizza['ingredients']) ;  ?></div>
+                        </div>
+
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 
     <?php include('./templates/footer.php'); ?>
 </html>
