0936d9f9ba50bc41f6ebc52f85ab0ab7125eff31
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-23 17:05:45 +0800

    error handling

--- add.php
+++ add.php
@@ -7,21 +7,33 @@ if (isset($_POST['submit'])) {
     if (empty($_POST['email'])) {
         echo 'An email is required <br />';
     } else {
-        echo htmlspecialchars($_POST['email']);
+        // echo htmlspecialchars($_POST['email']);
+        $email = $_POST['email'];
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "email must be a valid email address";
+        }
     }
 
     // check title
     if (empty($_POST['title'])) {
         echo 'An title is required <br />';
     } else {
-        echo htmlspecialchars($_POST['title']);
+        // echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+        if(!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            echo "Title must be letters and spaces only";
+        }
     }
 
     // check ingredients
     if (empty($_POST['ingredients'])) {
         echo 'An ingredients is required <br />';
     } else {
-        echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['ingredients'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            echo "Ingredients must be comma and separated list";
+        }
     }
 }
 ?>
